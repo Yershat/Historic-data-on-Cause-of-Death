116421dd0abf207cd1457585c0b7c908c1e7fd0e
Yershat Kuanysh <yershat.kuanysh@gmail.com>
2022-11-27 23:42:00 +0600

    dynamic part has been added

--- dynamic_pie_chart.py
+++ dynamic_pie_chart.py
@@ -1,23 +1,13 @@
+import random
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+labels=['a','b']
+for i in range(10):    
+    nums=[random.randint(5,10),random.randint(5,10)]
+    ax1.clear()
+    ax1.pie(nums, labels=labels, autopct='%1.1f%%', shadow=True, startangle=140)
+    plt.pause(2)
+plt.draw()
 
-colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'limegreen', 
-          'red', 'navy', 'blue', 'magenta', 'crimson']
-explode = (0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, .01)
-labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-nums = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-fig, ax = plt.subplots()
-
-def update(num):
-    ax.clear()
-    ax.axis('equal')
-    str_num = str(num)
-    for x in range(10):
-        nums[x] += str_num.count(str(x))
-    ax.pie(nums, explode=explode, labels=labels, colors=colors, 
-            autopct='%1.1f%%', shadow=True, startangle=140)
-    ax.set_title(str_num)
-
-ani = FuncAnimation(fig, update, frames=range(100), repeat=False)
-plt.show()
--- my_dynamic_chart.py
+++ my_dynamic_chart.py
@@ -3,6 +3,7 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 data = pd.read_csv("C:/Users/KYershat/Desktop/projects/cause_of_deaths_around_the_world/cause_of_deaths.csv")
 
 # I want to get top 10 causes of death data for each year and each country(country should be avaible as a choice)
@@ -12,46 +13,80 @@ data["Total"] = data.iloc[:,3:].sum(axis=1)
 
 for col in data.columns[3:-1]:
     data[col] /= data["Total"]
-    data[col] = round(data[col],3)
+    data[col] = round(data[col],6)
 
 labels = []
+
 for i in data:
     labels.append(i)
 
 causes = labels[3:-1]
 
-
-percentage_of_cause = []
-for i in data.iloc[1,3:-1]:
-    percentage_of_cause.append(i)
-
-ten_main_causes = sorted(percentage_of_cause,reverse = True)[0:10]
-
-## Location of diseases with highest percentage in the original row
-location_of_causes = []
-for i in ten_main_causes:
-    for a,ind in enumerate(percentage_of_cause):
-        if i ==ind:
-            location_of_causes.append(a)
-
-## Adding the remaining causes as other 
-other = 1 - sum(ten_main_causes)
-ten_main_causes.append(other)
-## Identifying the main diseases names from original row
-causes_names= []
-for i in location_of_causes:
-    causes_names.append(causes[i])
-causes_names.append("Other")
-explode = (0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.2,0.3,0.3,0.2,0.2)
-colors= ['red', 'orange', 'blue', 'white', 'green', 'yellow', 'brown','pink','#ff9999','#66b3ff','#99ff99','#ffcc99']
-
 country = "Zimbabwe"
 
-## How to go over each row with country and year above it?
 
+
+## Animating the pie chart
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+
+start_year = 1979
 for i,row in data.loc[data["Country/Territory"] == country].iterrows():
-    a = data.loc[i]
-    print(a.iloc[1,3:-1])
+    percentage_of_cause = []
+
+    for a in row[3:-1]:
+        percentage_of_cause.append(a)
+
+    
+    
+    ten_main_causes = sorted(percentage_of_cause,reverse = True)[0:10]
+
+    
+
+    location_of_causes = []
+
+    for i in ten_main_causes:
+        for a,ind in enumerate(percentage_of_cause):
+            if i == ind:
+                location_of_causes.append(a)
+
+    
+    
+    other = 1 - sum(ten_main_causes)
+
+    ten_main_causes.append(other)
+
+    
+
+    causes_names= []
+
+    for i in location_of_causes:
+        causes_names.append(causes[i])
+    
+    causes_names.append("Other")
+
+    
+
+    
+    explode = (0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.2,0.3,0.3,0.2,0.2)
+
+    colors= ['orange', 'blue', 'white', 'green', 'yellow', 'brown','pink','#ff9999','#66b3ff','#99ff99','#ffcc99']
+
+    ax1.clear()
+    ax1.pie(ten_main_causes, labels=causes_names, autopct='%1.1f%%', shadow=True, startangle=140,colors = colors)
+    
+    plt.pause(0.5)
+    percentage_of_cause.clear()
+    ten_main_causes.clear()
+    location_of_causes.clear()
+    causes_names.clear()
+
+plt.draw()
+
+
+        
 
 # plt.pie(ten_main_causes, labels = causes_names,autopct='%1.1f%%',explode=explode,colors = colors)
 # plt.show() 
