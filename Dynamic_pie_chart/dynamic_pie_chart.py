19079fad9e1a88fa7c59e6191f9a555123af66e7
Yershat Kuanysh <yershat.kuanysh@gmail.com>
2022-12-01 13:58:43 +0600

    change of plans regarding what to show

--- Dynamic_pie_chart/dynamic_pie_chart.py
+++ Dynamic_pie_chart/dynamic_pie_chart.py
@@ -26,7 +26,7 @@ for i in data:
 causes = labels[3:-1]
 
 # Country can be chosen by user 
-country = "Kazakhstan"
+country = "India"
 
 
 
@@ -84,7 +84,7 @@ for i,row in data.loc[data["Country/Territory"] == country].iterrows():
     plt.text(-0.3, 1.05, the_text_on_title, ha='left', va='top', transform=ax1.transAxes)
     ax1.pie(ten_main_causes, labels=causes_names, autopct=my_autopct, shadow=True, startangle=140,colors = colors, rotatelabels=True)
     start_year+=1
-    plt.pause(0.8)
+    plt.pause(1)
     percentage_of_cause.clear()
     ten_main_causes.clear()
     location_of_causes.clear()
